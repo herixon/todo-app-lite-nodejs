e171e5e949698faf152cf6654d3df82fea82ce00
herixon <111833369+herixon@users.noreply.github.com>
2022-08-23 17:13:32 +0900

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -3,9 +3,9 @@ const mongoose =require("mongoose");
 const TaskSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true,"タスク名を入力してください"],
+        required: [true,"何かを入力してください"],
         trim: true,
-        maxlength:[20, "タスク名は20文字以内"],
+        maxlength:[20, "文字数は20文字以内"],
     },
     completed:{
         type:Boolean,
